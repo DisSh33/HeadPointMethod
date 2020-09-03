7100f771306249949d2cc28e44f01ed0b1c638d5
diS <36980493+DisSh33@users.noreply.github.com>
2020-09-03 11:40:33 +0300

    Add files via upload

---
+++ AreaCalculation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadPointMethod
+{
+    public class AreaCalculation
+    {
+
+        public double Area(double a, double b, double c)
+        {
+            var input = new List<double>();
+            if (a > 0 && b > 0 && c > 0)
+            {
+                input.Add(a);
+                input.Add(b);
+                input.Add(c);
+            }
+            else
+            {
+                return 0;
+            }
+
+            input.Sort();
+
+            if (input[2] == Math.Sqrt(Math.Pow(input[0], 2) + Math.Pow(input[1], 2)))
+            {
+                return (input[0] * input[1]) / 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ AreaCalculationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HeadPointMethod.Tests
+{
+    public class AreaCalculationTests
+    {
+        [Theory]
+        [InlineData(0, 7, 8.88, 0)]
+        [InlineData(3, 4, 7, 0)]
+        [InlineData(-7, 5 , 10 , 0)]
+        [InlineData(3, 4, 5, 6)]
+        [InlineData(15, 20, 25, 150)]
+        public void Area_TestCalculations(double a, double b, double c, double expected)
+        {
+            var calculation = new AreaCalculation();
+
+            double actual = calculation.Area(a, b, c);
+
+            Assert.Equal(actual, expected);
+        }
+    }
+}
